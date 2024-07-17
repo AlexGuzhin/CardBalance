b06a57e11f8873a41b2a7146c23444e9e97a4a20
Alex Guzhin <growfood1989@yandex.ru>
2024-07-17 15:43:12 +0300

    доработка

--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -1,12 +1,12 @@
 package ru.netology.web.test;
 
-import lombok.var;
+import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.web.data.DataHelper;
 import ru.netology.web.page.DashboardPage;
+import ru.netology.web.page.LoginPage;
 
-import static java.nio.channels.FileChannel.open;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static ru.netology.web.data.DataHelper.*;
@@ -20,7 +20,7 @@ public class MoneyTransferTest {
 
     @BeforeEach
     void setup() {
-        var loginPage = ("http://localhost:9999", loginPage.class);
+        var loginPage = Selenide.open("http://localhost:9999", LoginPage.class);
         var authInfo = DataHelper.getAuthInfo();
         var verificationPage = loginPage.validLogin(authInfo);
         var verificationCode = DataHelper.getVerificationCode();
@@ -34,7 +34,7 @@ public class MoneyTransferTest {
     @Test
     void shouldTransferFromFirstToSecond() {
         var amount = generateValidAmount(firstCardBalance);
-        var expectedBalanceFirstCard = firstCardBalance + amount;
+        var expectedBalanceFirstCard = firstCardBalance - amount;
         var expectedBalanceSecondCard = secondCardBalance + amount;
         var transferPage = dashboardPage.selectCardToTransfer(secondCardInfo);
         dashboardPage = transferPage.makeValidTransfer(String.valueOf(amount), firstCardInfo);
